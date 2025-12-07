49a95c6c0fb8ec455355af0342e8c24e54fd59ea
hanjunyi <wuyuhuahen@gmail.com>
2025-12-07 22:34:31 +0900

    feat: Add initial auth schema and migration setup

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,8 +1,14 @@
 import "server-only";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 /**
  * データベース
  */
-export const db = drizzle(process.env.DATABASE_URL ?? "");
+export const db = drizzle({ client: pool, schema });
